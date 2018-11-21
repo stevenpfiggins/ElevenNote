fdb1760e2c0098837eb3e9a171489f201d81dc26
stevenpfiggins <sfiggins43@gmail.com>
2018-11-21 13:02:38 -0500

    Added NoteCreate Class and Create view. Altered ToString() Override

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -16,5 +16,11 @@ namespace ElevenNote.WebMVC.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
