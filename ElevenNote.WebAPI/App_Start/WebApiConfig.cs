cf8575cc12b307a2a11c6e7da8e82ffcc1cde6ce
stevenpfiggins <sfiggins43@gmail.com>
2018-11-27 09:52:28 -0500

    Enabled Cors

--- ElevenNote.WebAPI/App_Start/WebApiConfig.cs
+++ ElevenNote.WebAPI/App_Start/WebApiConfig.cs
@@ -17,6 +17,8 @@ namespace ElevenNote.WebAPI
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
+            config.EnableCors();
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
